a64b9c3971f506427c1f0eb50aac547f314570d9
PENG AO <pengao1293@gmail.com>
2022-09-13 14:14:53 +0900

    feat: post-04 done

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,7 +1,7 @@
-// main.rs
+// basic_boot.rs
 
-#![no_std] // block standard lib
-#![no_main] // reset entry point
+#![no_std]
+#![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(rustos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
@@ -11,23 +11,16 @@ use rustos::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World! at {}", "12:21");
-
-    #[cfg(test)]
     test_main();
-
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler] // normal one
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rustos::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
